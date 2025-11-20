58b33afe4ff7c3ad3c694dac2455ebca287612ee
dblitz29 <primafadhil.sulistyo@gmail.com>
2025-11-20 10:26:01 +0700

    2.1 - Answer question no 2

---
+++ 2.py
@@ -0,0 +1,25 @@
+# this code reads the large csv in chunks so it doesnt take  much memory.
+# with chunks, I can still calculate totals and summary without storing everything. O(n)
+
+import pandas as pd
+
+file_path = 'assets\\customers-200000.csv'
+chunk_size = 50000
+total_rows = 0
+country_count = {}
+city_count = {}
+
+for chunk in pd.read_csv(file_path, chunksize=chunk_size):
+    total_rows += len(chunk)
+    for country in chunk['Country']:
+        if country in country_count:
+            country_count[country] += 1
+        else:
+            country_count[country] = 1
+
+    for city in chunk['City']:
+        if city in city_count:
+            city_count[city] += 1
+        else:
+            city_count[city] = 1
+
